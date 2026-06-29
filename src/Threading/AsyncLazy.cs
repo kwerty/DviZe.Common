46043ab3c838248b90c533537db1c2e01490783f
kwerty <tim@kwerty.com>
2026-06-29 17:58:14 +1200

    Refactor AsyncLazy.

--- src/Threading/AsyncLazy.cs
+++ src/Threading/AsyncLazy.cs
@@ -4,49 +4,106 @@ using System.Threading.Tasks;
 
 namespace Kwerty.DviZe.Threading;
 
-public sealed partial class AsyncLazy<T>
+public sealed class AsyncLazy<T>
 {
     readonly Lock lockObj = new();
     readonly Func<CancellationToken, Task<T>> valueFactory;
     readonly bool canRetry;
-    Session session;
+    Request currRequest;
 
     public AsyncLazy(Func<CancellationToken, Task<T>> valueFactory, bool canRetry = false)
     {
         ArgumentNullException.ThrowIfNull(valueFactory, nameof(valueFactory));
+
         this.valueFactory = valueFactory;
         this.canRetry = canRetry;
-    }
-
-    Session GetSession()
-    {
-        lock (lockObj)
-        {
-            if (session == null
-                || session.Closed)
-            {
-                session = new Session(valueFactory, canRetry);
-            }
-
-            return session;
-        }
     }
 
     public async Task<T> GetValueAsync(CancellationToken cancellationToken = default)
     {
         while (true)
         {
-            var session = GetSession();
+            Request request;
+
+            lock (lockObj)
+            {
+                if (currRequest == null
+                    || currRequest.Result.IsCanceled
+                    || (currRequest.Result.IsFaulted && canRetry))
+                {
+                    currRequest = new Request(valueFactory);
+                }
+
+                request = currRequest;
+            }
+
+            using var _ = request.RegisterVotingToken(cancellationToken);
 
             try
             {
-                return await session.GetResultAsync(cancellationToken).ConfigureAwait(false);
+                return await request.WaitForResultAsync().ConfigureAwait(false);
             }
-            catch (SessionClosedException)
+            catch (OperationCanceledException) when (request.Result.IsCanceled)
             {
+                cancellationToken.ThrowIfCancellationRequested();
             }
         }
     }
 
-    sealed class SessionClosedException : Exception;
+    sealed class Request(Func<CancellationToken, Task<T>> valueFactory)
+    {
+        readonly CancellationTokenSource cts = new();
+        readonly TaskCompletionSource<T> resultSrc = new();
+        bool initialized;
+        int voteCount; // Represents the number of uncanceled voting tokens.
+
+        public Task<T> Result => resultSrc.Task;
+
+        public IDisposable RegisterVotingToken(CancellationToken votingToken)
+        {
+            Interlocked.Increment(ref voteCount);
+
+            return votingToken.Register(() =>
+            {
+                if (Interlocked.Decrement(ref voteCount) == 0)
+                {
+                    // No-op if already canceled/completed.
+
+                    try
+                    {
+                        cts.Cancel();
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                    }
+                }
+            });
+        }
+
+        public async Task<T> WaitForResultAsync()
+        {
+            if (!Interlocked.Exchange(ref initialized, true))
+            {
+                try
+                {
+                    var result = await valueFactory(cts.Token).ConfigureAwait(false);
+                    resultSrc.SetResult(result);
+                }
+                catch (OperationCanceledException) when (cts.Token.IsCancellationRequested)
+                {
+                    resultSrc.SetCanceled(cts.Token);
+                }
+                catch (Exception ex)
+                {
+                    resultSrc.SetException(ex);
+                }
+                finally
+                {
+                    cts.Dispose();
+                }
+            }
+
+            return await Result.ConfigureAwait(false);
+        }
+    }
 }
