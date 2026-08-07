c110f962aedb762fbde75293f4a4e83e39cac74e
kwerty <tim@kwerty.com>
2026-08-07 18:12:24 +1200

    Added KeyValuePairLogState and logging extension methods.

---
+++ src/Logging/KeyValuePairLogState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Kwerty.DviZe.Logging;
+
+public class KeyValuePairLogState(List<KeyValuePair<string, object>> pairs = null)
+    : IReadOnlyList<KeyValuePair<string, object>>
+{
+    List<KeyValuePair<string, object>> pairs = pairs;
+    bool hasComputed;
+
+    List<KeyValuePair<string, object>> GetKeyValuePairs()
+    {
+        if (!hasComputed)
+        {
+            hasComputed = true;
+            pairs ??= [];
+            OnComputingKeyValuePairs(pairs);
+        }
+        return pairs;
+    }
+
+    IEnumerator<KeyValuePair<string, object>> IEnumerable<KeyValuePair<string, object>>.GetEnumerator()
+        => GetKeyValuePairs().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => ((IEnumerable<KeyValuePair<string, object>>)this).GetEnumerator();
+
+    KeyValuePair<string, object> IReadOnlyList<KeyValuePair<string, object>>.this[int index]
+        => GetKeyValuePairs()[index];
+
+    int IReadOnlyCollection<KeyValuePair<string, object>>.Count
+        => GetKeyValuePairs().Count;
+
+    protected virtual void OnComputingKeyValuePairs(IList<KeyValuePair<string, object>> pairs) { }
+}
---
+++ src/Logging/LoggerExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Kwerty.DviZe.Logging;
+
+public static class LoggerExtensions
+{
+    extension(ILogger logger)
+    {
+        public void LogTrace<TState>(TState state, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Trace, eventId: default, state, exception: null, formatter);
+        }
+
+        public void LogDebug<TState>(TState state, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Debug, eventId: default, state, exception: null, formatter);
+        }
+
+        public void LogInformation<TState>(TState state, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Information, eventId: default, state, exception: null, formatter);
+        }
+
+        public void LogWarning<TState>(TState state, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Warning, eventId: default, state, exception: null, formatter);
+        }
+
+        public void LogError<TState>(TState state, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Error, eventId: default, state, exception: null, formatter);
+        }
+
+        public void LogCritical<TState>(TState state, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Critical, eventId: default, state, exception: null, formatter);
+        }
+
+        public void LogTrace<TState>(TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Trace, eventId: default, state, exception, formatter);
+        }
+
+        public void LogDebug<TState>(TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Debug, eventId: default, state, exception, formatter);
+        }
+
+        public void LogInformation<TState>(TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Information, eventId: default, state, exception, formatter);
+        }
+
+        public void LogWarning<TState>(TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Warning, eventId: default, state, exception, formatter);
+        }
+
+        public void LogError<TState>(TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Error, eventId: default, state, exception, formatter);
+        }
+
+        public void LogCritical<TState>(TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(LogLevel.Critical, eventId: default, state, exception, formatter);
+        }
+    }
+}
+
